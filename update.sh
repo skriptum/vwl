ed6bd570edac4f3323257a26da0308ff6e57355d
martenwalksaway <77919093+skriptum@users.noreply.github.com>
2023-05-17 16:04:04 +0200

    Update: 2023-05-17

--- update.sh
+++ update.sh
@@ -1,15 +1,15 @@
 echo "fetching subtrees"
 git fetch vwl1 master
-git subtree pull --prefix ./vwl1 vwl1 vwl1 main --squash
+git subtree pull --prefix vwl1 vwl1 main --squash
 
 git fetch vwl2 master
-git subtree pull --prefix ./vwl2 vwl2 vwl2 main --squash
+git subtree pull --prefix  vwl2 vwl2 main --squash
 
 git fetch vwl3 master
-git subtree pull --prefix ./vwl3 vwl3 vwl3 master --squash
+git subtree pull --prefix vwl3 vwl3 master --squash
 
 git fetch vwl4 master
-git subtree pull --prefix ./vwl4 vwl4 vwl4 master --squash
+git subtree pull --prefix vwl4 vwl4 master --squash
 
 echo "updating TOC"
 python3 TOC.py
